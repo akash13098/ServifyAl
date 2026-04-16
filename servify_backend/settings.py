a7cb5e493234e2bde30cea21e243efeaa230ee23
Akash <akashpatange098@gmail.com>
2026-04-16 10:36:55 +0530

    connected DB

--- servify_backend/settings.py
+++ servify_backend/settings.py
@@ -73,9 +73,13 @@ WSGI_APPLICATION = 'servify_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'servifyai_db',
+        'USER': 'servifyai_user',
+        'PASSWORD': 'Strong@123',
+        'HOST': '192.168.0.253',
+        'PORT': '3306',
     }
 }
 
